df86b6bb0009e355104c788b40b79f338cdd28df
Kevin Deems <kevin8deems@gmail.com>
2023-06-27 01:22:37 -0400

    Build out get set del and ping
    * Abstract out error handling and response parsing
    * Test each command in preparation for scheduler

--- pkg/redis/client.go
+++ pkg/redis/client.go
@@ -11,13 +11,20 @@ const (
 	DialTimeout   time.Duration = time.Second * 2
 	ConnRetryWait time.Duration = time.Millisecond * 10
 
+	MaxBatch int = 10
+
 	DefaultNetwork string = "tcp"
 )
 
 const (
+	ErrorType string = "err"
+)
+
+const (
 	InvalidAddrErr string = "address host:port are invalid"
-	EmptyKeyErr    string = "key cannot be empty on request"
-	EmptyValErr    string = "value cannot be empty on request"
+	EmptyParamErr  string = "parameters cannot be empty on request"
+	EmptyResErr    string = "empty response back from %s request"
+	EmptyResArgErr string = "empty argument from %s request"
 
 	ClientUninitializedErr string = "client was not initialized"
 	ClientInitTimeoutErr   string = "timed out dialing %s for %s"
@@ -26,6 +33,9 @@ const (
 type Client struct {
 	url  string
 	conn net.Conn
+
+	shutdown chan bool
+	requests chan clientReq
 }
 
 func NewClient(host string, port int) (*Client, error) {
@@ -48,6 +58,8 @@ func NewClient(host string, port int) (*Client, error) {
 		return nil, err
 	}
 
+	c.initChannels()
+	// go c.scheduler()
 	return c, nil
 }
 
@@ -75,32 +87,118 @@ func connectWithTimeout(addr string, timeout time.Duration) (net.Conn, error) {
 	}
 }
 
+func (c *Client) initChannels() {
+	c.shutdown = make(chan bool, 1)
+}
+
 func (c *Client) Ping() error {
 	if err := c.validateClient(); err != nil {
 		return err
 	}
 
-	return nil
+	return c.pingRequest()
 }
 
-func (c *Client) Get(k string) (string, error) {
-	var (
-		err error
-	)
-	if err = c.validateGet(k); err != nil {
+type clientReq struct {
+	req []string
+	res chan []string
+}
+
+func (c *Client) pingRequest() error {
+	response := c.sendRequest(PING)
+	return expectResponse(PING, PONG, response)
+}
+
+func (c *Client) sendRequest(args ...string) []string {
+	resChan := make(chan []string)
+	c.requests <- clientReq{
+		req: args,
+		res: resChan,
+	}
+	return <-resChan
+}
+
+func errorResponse(command string, res []string) error {
+	if len(res) == 0 {
+		return fmt.Errorf(EmptyResErr, command)
+	}
+
+	first := res[0]
+	if len(first) == 0 {
+		return fmt.Errorf(EmptyResArgErr, command)
+	}
+
+	if first[0] != Error {
+		return nil
+	}
+
+	return fmt.Errorf(first[1:])
+}
+
+func (c *Client) Get(key string) (string, error) {
+	if err := c.validateParams(key); err != nil {
 		return "", err
 	}
 
-	return "", nil
+	response := c.sendRequest(GET, key)
+	return getResponse(key, response)
 }
 
-func (c *Client) validateGet(k string) error {
+func getResponse(key string, res []string) (string, error) {
+	if err := errorResponse(GET, res); err != nil {
+		return "", err
+	}
+
+	if len(res) != 1 || res[0] == "" {
+		return "", fmt.Errorf(
+			"expected value for key %s, received %d results: %v", key, len(res), res,
+		)
+	}
+
+	return res[0], nil
+}
+
+func (c *Client) Set(key, val string) error {
+	if err := c.validateParams(key, val); err != nil {
+		return err
+	}
+
+	response := c.sendRequest(SET, key, val)
+	return expectResponse(SET, OK, response)
+}
+
+func (c *Client) validateParams(params ...string) error {
 	if err := c.validateClient(); err != nil {
 		return err
 	}
 
-	if k == "" {
-		return errors.New(EmptyKeyErr)
+	for _, param := range params {
+		if param == "" {
+			return errors.New(EmptyParamErr)
+		}
+	}
+
+	return nil
+}
+
+func (c *Client) Del(key string) error {
+	if err := c.validateParams(key); err != nil {
+		return err
+	}
+
+	response := c.sendRequest(DEL, key)
+	return expectResponse(DEL, OK, response)
+}
+
+func expectResponse(command, expected string, res []string) error {
+	if err := errorResponse(command, res); err != nil {
+		return err
+	}
+
+	if len(res) != 1 || res[0] != expected {
+		return fmt.Errorf(
+			"expected %s for %s, received %v", expected, command, res,
+		)
 	}
 
 	return nil
@@ -114,13 +212,100 @@ func (c *Client) validateClient() error {
 	return nil
 }
 
+// func createSimpleString(msg string) string {
+// 	var (
+// 		msgLength = strconv.Itoa(len(msg))
+// 		builder   strings.Builder
+// 	)
+// 	builder.WriteByte(BulkString)
+// 	builder.WriteString(msgLength)
+// 	builder.WriteString(NewLine)
+// 	builder.WriteString(msg)
+// 	builder.WriteString(NewLine)
+// 	return builder.String()
+// }
+
 func (c *Client) TearDown() error {
 	err := c.conn.Close()
-	return err
+	if err != nil {
+		return err
+	}
+
+	c.shutdown <- true
+	return nil
 }
 
-/*
-thread 1 - main client, sends serial requests to server
-- constantly waiting for requests
-thread 2 - scheduler (deduplicates requests)
-*/
+// func (c *Client) scheduler() {
+// 	bulkRequest, resChannels := c.aggregateRequests(true)
+// 	for {
+// 		select {
+// 		case <-c.shutdown:
+// 			fmt.Println("Shutting down scheduler")
+// 			return
+// 		default:
+// 			c.conn.Write(bulkRequest)
+// 			bulkRequest, resChannels = c.aggregateRequests(false)
+// 			bulkResponse := c.read()
+// 			c.partitionResponses(bulkResponse, resChannels)
+// 		}
+// 	}
+// }
+
+// func (c *Client) read() []byte {
+// 	// c.conn.Read()
+// 	return []byte{}
+// }
+
+// func (c *Client) aggregateRequests(blocking bool) ([]byte, [][]chan string) {
+// 	seen := map[string]int{}
+// 	resChannels := [][]chan string{}
+// 	s := make([]byte, 0)
+
+// 	var (
+// 		req        clientReq
+// 		requestInx int
+// 	)
+
+// 	if blocking {
+// 		req = <-c.requests
+// 	}
+
+// 	for req = range c.requests {
+// 		requestData := req.data
+// 		duplicateInx, dup := seen[requestData]
+// 		if !dup {
+// 			seen[requestData] = requestInx
+
+// 		} else {
+// 			resChannels[duplicateInx] = append(resChannels[duplicateInx], req.res)
+// 			resChannels = append(resChannels)
+// 		}
+// 		s = append(s, i)
+// 		requestInx++
+// 	}
+
+// 	s = addBulkHeader(s, requestInx)
+// 	return s, resChannels
+// }
+
+// func addBulkHeader(s []byte, numRequests int) []byte {
+// 	var (
+// 		buffer bytes.Buffer
+// 	)
+
+// 	buffer.WriteByte(Array)
+// 	buffer.WriteString(strconv.Itoa(numRequests))
+// 	buffer.WriteString(NewLine)
+// 	buffer.Write(s)
+// 	return buffer.Bytes()
+// }
+
+// func (c *Client) partitionResponses(responses []byte, channels [][]chan string) {
+
+// }
+
+// /*
+// thread 1 - main client, sends serial requests to server
+// - constantly waiting for requests
+// thread 2 - scheduler (deduplicates requests)
+// */
--- pkg/redis/client_test.go
+++ pkg/redis/client_test.go
@@ -1,6 +1,7 @@
 package redis
 
 import (
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,4 +16,254 @@ func TestClientInit(t *testing.T) {
 	c, err := NewClient(host, port)
 	require.NoError(t, err)
 	require.NotNil(t, c, "client shouldn't be nil")
+	err = c.TearDown()
+	require.NoError(t, err)
+}
+
+func setupClient() *Client {
+	return &Client{
+		requests: make(chan clientReq),
+		url:      "localhost:1234",
+		conn:     &net.TCPConn{},
+	}
+}
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name              string
+		key               string
+		response          string
+		failBeforeRequest bool
+		check             func(*testing.T, string, error)
+	}{
+		{
+			name:     "valid get",
+			key:      "key",
+			response: "data",
+			check: func(t *testing.T, v string, err error) {
+				require.NoError(t, err)
+				require.Equal(t, "data", v)
+			},
+		},
+		{
+			name:     "expect error on invalid get response",
+			key:      "key",
+			response: "",
+			check: func(t *testing.T, _ string, err error) {
+				require.Error(t, err)
+			},
+		},
+		{
+			name:              "empty key",
+			failBeforeRequest: true,
+			check: func(t *testing.T, _ string, err error) {
+				require.Error(t, err)
+				require.Equal(t, EmptyParamErr, err.Error())
+			},
+		},
+		{
+			name:     "error response",
+			key:      "key",
+			response: "-Invalid get",
+			check: func(t *testing.T, _ string, err error) {
+				require.Equal(t, "Invalid get", err.Error())
+			},
+		},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := setupClient()
+			if !tt.failBeforeRequest {
+				go func() {
+					req := <-c.requests
+					require.Equal(t, []string{GET, tt.key}, req.req)
+					req.res <- []string{tt.response}
+				}()
+			}
+			val, err := c.Get(tt.key)
+			tt.check(t, val, err)
+		})
+	}
+}
+
+func TestSet(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name              string
+		key               string
+		val               string
+		response          string
+		failBeforeRequest bool
+		check             func(*testing.T, error)
+	}{
+		{
+			name:     "valid set",
+			key:      "key",
+			val:      "val",
+			response: OK,
+			check: func(t *testing.T, err error) {
+				require.NoError(t, err)
+			},
+		},
+		{
+			name:     "expect error on invalid set response",
+			key:      "key",
+			val:      "val",
+			response: "",
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+			},
+		},
+		{
+			name:              "empty val",
+			key:               "key",
+			failBeforeRequest: true,
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+				require.Equal(t, EmptyParamErr, err.Error())
+			},
+		},
+		{
+			name:     "error response",
+			key:      "key",
+			val:      "val",
+			response: "-Invalid set",
+			check: func(t *testing.T, err error) {
+				require.Equal(t, "Invalid set", err.Error())
+			},
+		},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := setupClient()
+			if !tt.failBeforeRequest {
+				go func() {
+					req := <-c.requests
+					require.Equal(t, []string{SET, tt.key, tt.val}, req.req)
+					req.res <- []string{tt.response}
+				}()
+			}
+			tt.check(t, c.Set(tt.key, tt.val))
+		})
+	}
+}
+func TestDelete(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name              string
+		key               string
+		response          string
+		failBeforeRequest bool
+		check             func(*testing.T, error)
+	}{
+		{
+			name:     "valid delete",
+			key:      "delete",
+			response: OK,
+			check: func(t *testing.T, err error) {
+				require.NoError(t, err)
+			},
+		},
+		{
+			name:     "expect error on invalid del response",
+			key:      "delete",
+			response: PING,
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+			},
+		},
+		{
+			name:              "empty key",
+			failBeforeRequest: true,
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+				require.Equal(t, EmptyParamErr, err.Error())
+			},
+		},
+		{
+			name:     "error response",
+			key:      "delete",
+			response: "-Invalid delete",
+			check: func(t *testing.T, err error) {
+				require.Equal(t, "Invalid delete", err.Error())
+			},
+		},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := setupClient()
+			if !tt.failBeforeRequest {
+				go func() {
+					req := <-c.requests
+					require.Equal(t, []string{DEL, tt.key}, req.req)
+					req.res <- []string{tt.response}
+				}()
+			}
+			tt.check(t, c.Del(tt.key))
+		})
+	}
+}
+
+func TestPing(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name              string
+		response          string
+		failBeforeRequest bool
+		check             func(*testing.T, error)
+	}{
+		{
+			name:     "valid ping pong",
+			response: PONG,
+			check: func(t *testing.T, err error) {
+				require.NoError(t, err)
+			},
+		},
+		{
+			name:     "server responds PING expect error",
+			response: PING,
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+			},
+		},
+		{
+			name:     "error response",
+			response: "-Invalid request",
+			check: func(t *testing.T, err error) {
+				require.Equal(t, "Invalid request", err.Error())
+			},
+		},
+		{
+			name:              "invalid client initialization",
+			failBeforeRequest: true,
+			check: func(t *testing.T, err error) {
+				require.Error(t, err)
+			},
+		},
+	}
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var c *Client
+			if !tt.failBeforeRequest {
+				c = setupClient()
+				go func() {
+					req := <-c.requests
+					require.Equal(t, []string{PING}, req.req)
+					req.res <- []string{tt.response}
+				}()
+			} else {
+				c = &Client{}
+			}
+			tt.check(t, c.Ping())
+		})
+	}
 }
